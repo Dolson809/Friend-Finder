04057fa31bca76151327d943e838b229ed2b2823
Daniel <sanjose1994@gmail.com>
2019-04-20 11:15:06 -0700

    why wont this work

---
+++ app/data/friends.js
@@ -0,0 +1,174 @@
+var friends = [
+    {
+        "name": "Admiral Ackbar",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/6/6e/AckbarStanding.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+
+{
+    "name": "Padmé Amidala",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/Amidala.png/220px-Amidala.png",
+            "scores": [
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5
+            ]
+},
+
+{
+    "name": "Darth Bane",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/DarthBane-frontal.jpg/220px-DarthBane-frontal.jpg",
+            "scores": [
+                5,
+                2,
+                2,
+                2,
+                5,
+                3,
+                2,
+                5,
+                4,
+                4
+            ]
+},
+
+{
+    "name": "BB-8",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/3/39/BB-8%2C_Star_Wars_The_Force_Awakens.jpg",
+            "scores": [
+                5,
+                1,
+                4,
+                1,
+                5,
+                1,
+                2,
+                5,
+                5,
+                5
+            ]
+},
+
+{
+    "name": "Jar Jar Binksed",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/4/4b/Jjportrait.jpg/220px-Jjportrait.jpg",
+            "scores": [
+                3,
+                1,
+                1,
+                4,
+                3,
+                1,
+                1,
+                5,
+                2,
+                1
+            ]
+},
+
+{
+    "name": "C-3PO",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/C-3PO_droid.png/220px-C-3PO_droid.png",
+            "scores": [
+                2,
+                1,
+                2,
+                4,
+                3,
+                1,
+                2,
+                5,
+                2,
+                1
+            ]
+},
+
+{
+    "name": "Count Dooku",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Count_Dooku.png/220px-Count_Dooku.png",
+            "scores": [
+                1,
+                1,
+                1,
+                4,
+                5,
+                2,
+                2,
+                3,
+                4,
+                1
+            ]
+},
+
+{
+    "name": "Jyn Erso",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/Felicity_Jones_as_Jyn_Erso_%282016%29.jpg/220px-Felicity_Jones_as_Jyn_Erso_%282016%29.jpg",
+            "scores": [
+                5,
+                3,
+                3,
+                4,
+                5,
+                1,
+                1,
+                5,
+                4,
+                2
+            ]
+},
+
+{
+    "name": "Boba Fett",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/FettbobaJB.png/220px-FettbobaJB.png",
+            "scores": [
+                5,
+                2,
+                4,
+                3,
+                5,
+                1,
+                2,
+                5,
+                1,
+                1
+            ]
+},
+
+{
+    "name": "Jabba the Hutt",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv-i1TTgQUsmgwfvBuHbiAuRXUFuFXfdUyOS2UIkNYX0Pqyao8",
+            "scores": [
+                5,
+                2,
+                4,
+                5,
+                5,
+                4,
+                2,
+                1,
+                4,
+                1
+            ]
+}
+]
+
+module.exports = friends;
